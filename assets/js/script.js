8ed644271b35167fa2f7f5802cfefcf707d20397
Daniela Leal <falfada@gmail.com>
2024-04-14 15:05:54 +1000

    Created function to create city button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,18 @@
 const cityForm = document.getElementById("searchCity");
+const forecastContainer = document.getElementById("forecastContainer");
+const citiesContainer = document.getElementById("citiesContainer");
 
+// Creates the button
+function createCityButton(city){
+  const cityButton = document.createElement('button');
+  cityButton.classList.add("btn", "btn-light", "mt-1", "col-12");
+  cityButton.textContent = `${city}`;
+
+  citiesContainer.appendChild(cityButton);
+}
+function displayInfo(data){
+
+}
 function searchCity(event) {
   event.preventDefault();
   const city = document.getElementById("city").value;
@@ -15,23 +28,31 @@ function searchCity(event) {
         const apiCity = data.city.name;
 
         // Changing the background depending on the weather info
-        let weatherInfo = data.list[0].weather[0].main;
-        const weatherContainer = document.getElementById("weather_container");
-        weatherContainer.setAttribute("class", "text-white");
+        // let weatherInfo = data.list[0].weather[0].main;
+        // const weatherContainer = document.getElementById("weather_container");
+        // weatherContainer.setAttribute("class", "text-white");
 
-        if(weatherInfo === 'Thunderstorm'){
-          weatherContainer.style.backgroundImage = "url('../assets/img/thunderstorm.jpg')";
-        } else if(weatherInfo === 'Drizzle'){
-          weatherContainer.style.backgroundImage = "url('../assets/img/drizzle.jpg')";
-        } else if(weatherInfo === 'Rain'){
-          weatherContainer.style.backgroundImage = "url('../assets/img/rain.jpg')";
-        } else if(weatherInfo === 'Snow'){
-          weatherContainer.style.backgroundImage = "url('../assets/img/snow.jpg')";
-        } else if(weatherInfo === 'Clear'){
-          weatherContainer.style.backgroundImage = "url('../assets/img/clear.jpg')";
-        } else{
-          weatherContainer.style.backgroundImage = "url('../assets/img/clouds.jpg')";
-        }
+        // if (weatherInfo === "Thunderstorm") {
+        //   weatherContainer.style.backgroundImage =
+        //     "url('../assets/img/thunderstorm.jpg')";
+        // } else if (weatherInfo === "Drizzle") {
+        //   weatherContainer.style.backgroundImage =
+        //     "url('../assets/img/drizzle.jpg')";
+        // } else if (weatherInfo === "Rain") {
+        //   weatherContainer.style.backgroundImage =
+        //     "url('../assets/img/rain.jpg')";
+        // } else if (weatherInfo === "Snow") {
+        //   weatherContainer.style.backgroundImage =
+        //     "url('../assets/img/snow.jpg')";
+        // } else if (weatherInfo === "Clear") {
+        //   weatherContainer.style.backgroundImage =
+        //     "url('../assets/img/clear.jpg')";
+        // } else {
+        //   weatherContainer.style.backgroundImage =
+        //     "url('../assets/img/clouds.jpg')";
+        // }
+
+        const cityArray = [];
 
         // Creating the next five days for the forecast
         for (let i = 0; i <= 5; i++) {
@@ -39,25 +60,31 @@ function searchCity(event) {
           let nextDayNewFormat = nextDay.format("DD/MM/YYYY");
           let foundMatch = false;
 
-          // Looping through the data array to check if it matches to 5-day forecast
+          // Looping through the data array to check if it matches to store the 5-day forecast
           for (let j = 0; j < data.list.length; j++) {
             let apiDateItem = data.list[j].dt_txt;
             let apiDateItemNewFormat = dayjs(apiDateItem).format("DD/MM/YYYY");
 
             if (apiDateItemNewFormat === nextDayNewFormat && !foundMatch) {
-              const dataList = data.list[j];
-              const humidity = dataList.main.humidity;
-              const temperature = dataList.main.temp;
-              const windSpeed = dataList.wind.speed;
-              const iconWeather = `https://openweathermap.org/img/wn/${dataList.weather[0].icon}.png`;
+              cityArray.push(data.list[j]);
 
-              
               foundMatch = true;
             }
           }
         }
+
+        // Creates the city button
+        createCityButton(apiCity);
+
+        // Display info
+        displayInfo(cityArray);
+
+        // Saves the information to localStorage
+
       });
   }
 }
 
 cityForm.addEventListener("submit", searchCity);
+
+// Handle  display the information click when clicking the city button
